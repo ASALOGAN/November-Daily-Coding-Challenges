b77f7895232322a3321fd3ac72a7a92f9e3a2663
Ahmed Shoieb Aqhtar <ahmedshoiebaqhtar@gmail.com>
2024-11-17 18:58:59 +0530

    Added solutions and notes for Day 29 17-11-24

--- Day 29 17-11-24/Leet Code/solution-1.js
+++ Day 29 17-11-24/Leet Code/solution-1.js
@@ -1,3 +1,12 @@
+// Problem: Coin Change
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+// You may assume that you have an infinite number of each kind of coin.
+// Constraints:
+// 1 <= coins.length <= 12
+// 1 <= coins[i] <= 2^31 - 1
+// 0 <= amount <= 10^4
+
 const coinChange = (coins, amount) => {
     // Create an array to store the minimum coins needed for each amount
     const dp = new Array(amount + 1).fill(Infinity);
